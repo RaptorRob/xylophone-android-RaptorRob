152d99ec9a7533798df6fee02cb6a9f6ceda2e43
RGEHLIS <robert.gehlis@zalando.de>
2017-11-01 12:31:23 +0100

    final

--- app/src/main/java/com/londonappbrewery/xylophonepm/MainActivity.java
+++ app/src/main/java/com/londonappbrewery/xylophonepm/MainActivity.java
@@ -31,11 +31,18 @@ public class MainActivity extends AppCompatActivity {
             R.raw.note6_a,
             R.raw.note7_b};
 
-    private int soundArrayNumber;
     private boolean soundPoolReady;
     private AudioAttributes.Builder mAAB;
     private SoundPool.Builder mSPB;
     private SoundPool mSP;
+
+    private int mSoundIdC;
+    private int mSoundIdD;
+    private int mSoundIdE;
+    private int mSoundIdF;
+    private int mSoundIdG;
+    private int mSoundIdA;
+    private int mSoundIdB;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -45,7 +52,7 @@ public class MainActivity extends AppCompatActivity {
         //Create AudioAttribute.Builder and set attributes
         mAAB = new AudioAttributes.Builder();
         mAAB.setUsage(AudioAttributes.USAGE_MEDIA);
-        mAAB.setContentType(AudioAttributes.CONTENT_TYPE_SONIFICATION);
+        mAAB.setContentType(AudioAttributes.CONTENT_TYPE_MUSIC);
         AudioAttributes audioAttributes = mAAB.build();
 
 
@@ -58,24 +65,55 @@ public class MainActivity extends AppCompatActivity {
         mSP.setOnLoadCompleteListener(new SoundPool.OnLoadCompleteListener() {
             @Override
             public void onLoadComplete(SoundPool soundPool, int sampleId, int status) {
+                Log.d("This app", "Load Complete: ");
                 soundPoolReady = true;
             }
         });
+
+        // Load sound in onCreate()
+        mSoundIdC = mSP.load(getApplicationContext(), soundArray[0], PRIORITY);
+        mSoundIdD = mSP.load(getApplicationContext(), soundArray[1], PRIORITY);
+        mSoundIdE = mSP.load(getApplicationContext(), soundArray[2], PRIORITY);
+        mSoundIdF = mSP.load(getApplicationContext(), soundArray[3], PRIORITY);
+        mSoundIdG = mSP.load(getApplicationContext(), soundArray[4], PRIORITY);
+        mSoundIdA = mSP.load(getApplicationContext(), soundArray[5], PRIORITY);
+        mSoundIdB = mSP.load(getApplicationContext(), soundArray[6], PRIORITY);
 
     }
 
     // TODO: Add the play methods triggered by the buttons
     public void playSoundFromSoundPool(View v)throws Exception{
-        soundArrayNumber = Integer.parseInt(v.getTag().toString());
-        Log.d("This app", "playSoundFromSoundPool: " + soundArrayNumber);
-        mSP.load(getApplicationContext(), soundArray[soundArrayNumber], PRIORITY);
+        String soundIDFromButton = v.getTag().toString();
+        int soundToPlay = 1;
+        switch (soundIDFromButton){
+            case "mSoundIdC" :
+                soundToPlay = mSoundIdC;
+                break;
+            case "mSoundIdD" :
+                soundToPlay = mSoundIdD;
+                break;
+            case "mSoundIdE" :
+                soundToPlay = mSoundIdE;
+                break;
+            case "mSoundIdF" :
+                soundToPlay = mSoundIdF;
+                break;
+            case "mSoundIdG" :
+                soundToPlay = mSoundIdG;
+                break;
+            case "mSoundIdA" :
+                soundToPlay = mSoundIdA;
+                break;
+            case "mSoundIdB" :
+                soundToPlay = mSoundIdB;
+                break;
+        }
+
         if(soundPoolReady){
-            Log.d("ss", "playSoundFromSoundPool: " + soundPoolReady);
-            mSP.play(soundArray[soundArrayNumber],LEFT_VOLUME, RIGHT_VOLUME, PRIORITY, NO_LOOP, NORMAL_PLAY_RATE);
+            Log.d("This app", "playSoundFromSoundPool: " + soundIDFromButton);
+            mSP.play(soundToPlay, LEFT_VOLUME, RIGHT_VOLUME, PRIORITY, NO_LOOP, NORMAL_PLAY_RATE);
         }
 
     }
-
-
 
 }
